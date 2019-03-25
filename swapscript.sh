35ce42a29f819a33681385d17a69bc507e7fc752
georgeoshardo <georgeoshardo@gmail.com>
2019-03-25 16:11:35 +0000

    new file:   swapscript.sh

---
+++ swapscript.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+echo "A simple script to add temporary swap space"
+echo "How much swap to add? (10 gigabytes = 10G)"
+read swapamount
+sudo fallocate -l $swapamount /swapfile
+sudo chmod 600 /swapfile
+sudo mkswap /swapfile
+sudo swapon /swapfile
+sudo swapon --show
